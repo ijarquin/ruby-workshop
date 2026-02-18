07f60fe0c157b7ccaf4fe62718d7a3b653e71b56
Isaac Jarquin <isaac.jarquin@sky.uk>
2026-02-18 11:36:10 +0000

    api: add RecipeIngredient resource
    - add join table recipe_ingredients to create many to many relationship
    between recipes and ingredients table

--- my-recipes/api/db/schema.rb
+++ my-recipes/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_18_112908) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_18_113252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_18_112908) do
     t.datetime "created_at", null: false
     t.string "description"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "ingredient_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -32,4 +41,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_18_112908) do
     t.string "title"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
 end
