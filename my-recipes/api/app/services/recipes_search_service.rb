30de104fce44fb2af82403c18285457c76da8591
Isaac Jarquin <isaac.jarquin@sky.uk>
2026-02-19 11:11:34 +0000

    api: move the recipes search logic into a service
    - create RecipesSearchService and move the searching logic into it

---
+++ my-recipes/api/app/services/recipes_search_service.rb
@@ -0,0 +1,27 @@
+class RecipesSearchService
+  def initialize(ingredients)
+    @ingredients = ingredients
+  end
+
+  def with_matching_ingredients
+    return Recipe.none if @ingredients.empty?
+
+    Recipe.includes(:ingredients).where(id: scope_matching_all_terms.select(:id))
+  end
+
+  private
+
+  attr_reader :ingredients
+
+  def scope_matching_all_terms
+    ingredients.reduce(Recipe.all) do |s, term|
+      s.where(
+        "EXISTS (SELECT 1 FROM recipe_ingredients ri
+                     JOIN ingredients i ON i.id = ri.ingredient_id
+                     WHERE ri.recipe_id = recipes.id
+                     AND i.name ILIKE ?)",
+        "%#{term}%"
+      )
+    end
+  end
+end
