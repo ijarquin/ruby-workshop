10afe5aafa43176096b25d1fe09aa57ada90edeb
Isaac Jarquin <isaac.jarquin@sky.uk>
2026-02-20 21:02:17 +0000

    web: display Recipe card
    - background image
    - category badge overlaping image
    - recipe title on the botton of card
    - when hover over show card is clickable

--- my-recipes/web/next.config.ts
+++ my-recipes/web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagesvc.meredithcorp.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.allrecipes.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
