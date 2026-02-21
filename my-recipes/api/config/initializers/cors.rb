cad17a23be5818878a4f508c86e7f3e977e60123
Isaac Jarquin <isaac.jarquin@sky.uk>
2026-02-21 23:34:34 +0000

    doc: update main README.md to reflect changes added to web and api
    doc: add api response to documentation
    
    doc: update the documentation
    
    - finalize the documentation
    - fix a minor lint issue in api

--- my-recipes/api/config/initializers/cors.rb
+++ my-recipes/api/config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before(0, Rack::Cors) do
     resource(
       "*",
       headers: :any,
-      methods: [:get, :options, :head]
+      methods: [ :get, :options, :head ]
     )
   end
 end
