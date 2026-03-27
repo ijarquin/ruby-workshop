79dc9932d01de45e0fcb6c03087ef8e9c4ccbca7
Isaac Jarquin <isaac.jarquin@sky.uk>
2026-03-27 21:26:12 +0000

    web: add /recipes/favourites route

---
+++ my-recipes/web/app/recipes/favourites/page.tsx
@@ -0,0 +1,5 @@
+export default function Favourites() {
+  return (
+    <div>hola que tal</div>
+  );
+}
---
+++ my-recipes/web/app/recipes/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import HomeContent from "../../components/HomeContent";
+
+export default function Home() {
+  return (
+    <Suspense fallback={null}>
+      <HomeContent />
+    </Suspense>
+  );
+}
